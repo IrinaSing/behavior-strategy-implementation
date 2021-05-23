bf01480ee871758dc7503cf9836eea7b671d34ac
IrinaSing <irinasingh.mail@yandex.ru>
2021-05-24 00:17:33 +0200

    ternary

--- function-design/exercises/easy/testing-paths-sequential-conditionals.test.js
+++ function-design/exercises/easy/testing-paths-sequential-conditionals.test.js
@@ -11,32 +11,61 @@
  *  // false, false --> '00'
  *  // false, true --> '01'
  */
-const stub = () => {};
+const stub = (a = false, b = false) => {
+  return '';
+};
 
 /*
 
 */
 
+const toBinary = (a = false, b = false) => {
+  if (typeof a != "boolean") {
+    throw new TypeError ('a is not boolean')
+  };
+  if (typeof b != "boolean") {
+    throw new TypeError ('b is not boolean')
+  };
+  let binA = a === true ? "1" : "0";
+  let binB = b === true ? "1" : "0";
+  return binA + binB;
+};
+
+
 for (const solution of [
-  secretSolution,
+  // secretSolution,
   // stub,
+  toBinary
 ]) {
   // this function only 4 possible combinations of arguments
   //  it's possible test them all and have 100% confidence in the function
   describe(solution.name + ': converts two booleans to binary', () => {
     it('true, true --> "11"', () => {
-      const actual = solution(_, _);
-      expect(actual).toEqual(_);
+      const actual = solution(true, true);
+      expect(actual).toEqual("11");
     });
     it('true, false --> "10"', () => {
-      const actual = _;
+      const actual = solution(true, false);
       expect(actual).toEqual('10');
     });
     it('false, true --> "01"', () => {
-      const actual = _;
-      _;
+      const actual = solution(false, true);
+      expect(actual).toEqual('01');
     });
-    it('_', () => {});
+    it('false, false --> "00"', () => {
+      const actual = solution(false, false);
+      expect(actual).toEqual('00');
+    });
+    describe(solution.name + ' throws error if argument is undefined', () => {
+      it('if typeof of the 1st argument is undefined', () => {
+      const actual = solution(undefined, true);
+      expect(() => solution(actual)).toThrowError(new TypeError('a is not boolean'));
+    });
+      it('if typeof of the 2nd argument is undefined', () => {
+        const actual = solution(false, undefined);
+        expect(() => solution(actual)).toThrowError(new TypeError('b is not boolean'));
+      });
+  });
   });
 }
 
